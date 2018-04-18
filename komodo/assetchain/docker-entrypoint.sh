d423bb61ac902798fff30b1c6b149b15cb4cd1fd
ASKz <maxime@monaize.com>
2018-04-18 13:26:43 +0200

    feat: first working version for komodo

---
+++ komodo/assetchain/docker-entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -e
+
+KOMODO_DATA=$KOMODO_DATA/$AC_NAME
+
+if [ -z ${AC_NAME+x} ]; then
+  echo "You have to set AC_NAME environment variable to start this container"
+  exit 1;
+fi
+
+if [ $(echo "$1" | cut -c1) = "-" ]; then
+  echo "$0: assuming arguments for komodod"
+
+  set -- komodod "$@"
+fi
+
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "komodod" ]; then
+  mkdir -p "$KOMODO_DATA"
+  chmod 700 "$KOMODO_DATA" "/home/komodo/.zcash-params"
+  chown -R komodo "$KOMODO_DATA" "/home/komodo/.zcash-params"
+
+  if [ ! -f $KOMODO_DATA/$AC_NAME.conf ]; then
+    touch $KOMODO_DATA/$AC_NAME.conf
+  fi
+  
+  if [ -z "$(ls -A /home/komodo/.zcash-params)" ]; then
+    alias exit=return
+    gosu komodo /fetch-params.sh
+  fi
+
+  echo "$0: setting data directory to $KOMODO_DATA"
+
+  set -- "$@" -datadir="$KOMODO_DATA"
+
+  echo "$0: setting asset chain to $AC_NAME"
+
+  set -- "$@" -ac_name="$AC_NAME"
+
+
+fi
+
+if [ "$1" = "komodod" ] || [ "$1" = "komodo-cli" ] || [ "$1" = "komodo-tx" ]; then
+  echo
+  exec gosu komodo "$@"
+fi
+
+echo
+exec "$@"
---
+++ komodo/mainnet/docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -e
+
+if [ $(echo "$1" | cut -c1) = "-" ]; then
+  echo "$0: assuming arguments for komodod"
+
+  set -- komodod "$@"
+fi
+
+if [[ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "komodod" ]] && [ !$(echo "$1") = "-help" ]; then
+  mkdir -p "$KOMODO_DATA"
+  chmod 700 "$KOMODO_DATA" "/home/komodo/.zcash-params"
+  chown -R komodo "$KOMODO_DATA" "/home/komodo/.zcash-params"
+
+  if [ ! -f $KOMODO_DATA/komodo.conf ]; then
+    touch $KOMODO_DATA/komodo.conf
+  fi
+  
+  if [ -z "$(ls -A /home/komodo/.zcash-params)" ]; then
+    alias exit=return
+    gosu komodo /fetch-params.sh
+  fi
+
+  echo "$0: setting data directory to $KOMODO_DATA"
+
+  set -- "$@" -datadir="$KOMODO_DATA"
+fi
+
+if [ "$1" = "komodod" ] || [ "$1" = "komodo-cli" ] || [ "$1" = "komodo-tx" ]; then
+  echo
+  exec gosu komodo "$@"
+fi
+
+echo
+exec "$@"
